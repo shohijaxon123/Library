261f12759a9be12a6673619f326f21cb37219652
xsagatof <Ultimate123443@gmail.com>
2024-12-05 21:37:30 +0500

    Added book class

---
+++ src/Book.java
@@ -0,0 +1,51 @@
+public class Book {
+    private int ID;
+    private String title;
+    private String author;
+    private String genre;
+    private int quantity;
+    private boolean status;
+
+    public Book(int id, String title, String author, String genre, int num, boolean status){
+        ID = id;
+        quantity = num;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.status = status;
+    }
+
+    public void incrementQuantity() {
+        quantity++;
+    }
+
+
+    public int getID(){
+        return ID;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public Boolean getStatus(){
+        return status;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+
+
+    public void displayInfo(){
+        System.out.println(ID);
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(genre);
+        System.out.println(quantity);
+        System.out.println(status);
+    }
+}
